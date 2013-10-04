b3bf6cb1000e6e5b6ed73303d6e7b4ec68ba6f16
rcgfopl <rcgfopl@gmail.com>
2013-10-03 19:53:15 -0700

    added waitforstart and wait time functions

--- FunctionLibrary.c
+++ FunctionLibrary.c
@@ -8,6 +8,8 @@ void Forward(int distance, int power)
 		motor[mLeft] = power;
 		motor[mRight] = power;
 	}
+	motor[mLeft] = 0;
+	motor[mRight] = 0;
 }
 
 void Backward(int distance, int power)
@@ -20,6 +22,8 @@ void Backward(int distance, int power)
 		motor[mLeft] = -power;
 		motor[mRight] = -power;
 	}
+	motor[mLeft] = 0;
+	motor[mRight] = 0;
 }
 
 void Turn(int distance, int power, int dir)
@@ -31,5 +35,68 @@ void Turn(int distance, int power, int dir)
 	{
 		motor[mLeft] = power*dir;
 		motor[mRight] = -power*dir;
+	}
+	motor[mLeft] = 0;
+	motor[mRight] = 0;
+}
+
+void selectTime()
+{
+	while(!timeSelected)
+	{
+		nxtDisplayCenteredTextLine(2, "Time: %.1f", (waitTime));
+		if (nNxtButtonPressed == 1)
+		{
+			wait1Msec(500);
+			PlaySound(soundBlip);
+			waitTime += 2.5;
+		}
+		else if (nNxtButtonPressed == 2)
+		{
+			wait1Msec(500);
+			PlaySound(soundBlip);
+			waitTime -= 2.5;
+		}
+		if (waitTime > 10)
+		{
+			waitTime = 10;
+		}
+		else if (waitTime < 0)
+		{
+			waitTime = 0;
+		}
+		if(nNxtButtonPressed == 3)
+		{
+			wait1Msec(500);
+			timeSelected = true;
+		}
+	}
+}
+
+
+void startSelect()
+{
+	while(!startSelected)
+	{
+		nxtDisplayCenteredTextLine(2, "WaitForStart? %s", wait);
+		if (nNxtButtonPressed == 1)
+		{
+			wait1Msec(500);
+			PlaySound(soundBlip);
+			wait = "Yes";
+			waitSelected = true;
+		}
+		else if (nNxtButtonPressed == 2)
+		{
+			wait1Msec(500);
+			PlaySound(soundBlip);
+			wait = "No";
+			waitSelected = false;
+		}
+		if(nNxtButtonPressed == 3)
+		{
+			wait1Msec(500);
+			startSelected = true;
+		}
 	}
 }
